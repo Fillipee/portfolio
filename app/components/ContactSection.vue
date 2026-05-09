1476add7e4d47a691d07f8f44655ab446c461bef
Filipee <thefilipee@gmail.com>
2026-05-09 22:09:24 +0200

    feat: contact link

--- app/components/ContactSection.vue
+++ app/components/ContactSection.vue
@@ -10,7 +10,7 @@
 		>
 			Kontakt
 		</BaseHeading>
-		<div class="md:w-1/2">
+		<div class="md:w-1/2 border-b border-border pb-16">
 			<BaseHeading
 				level="h3"
 				variant="h2"
@@ -18,9 +18,25 @@
 			>
 				Pojďme spolu něco vymyslet
 			</BaseHeading>
-			<p class="font-mono text-muted border-b border-border pb-16">
+
+			<a
+				href="mailto:filipveprik@seznam.cz"
+				class="group inline-flex items-center gap-2 font-mono text-muted hover:text-primary transition-colors"
+			>
 				filipveprik@seznam.cz
-			</p>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="opacity-0 -translate-x-2 transition-all group-hover:opacity-100 group-hover:translate-x-0"
+				><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z" /><path d="m21.854 2.147-10.94 10.939" /></svg>
+			</a>
 		</div>
 	</section>
 </template>
